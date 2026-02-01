06cdc690caec77f27c4bcdc91959fb61fd817232
Gladie <gladie38@gmail.com>
2026-02-01 20:06:07 +0100

    SUMMONS NOT REMOVED

--- src/game/chars/CCharAct.cpp
+++ src/game/chars/CCharAct.cpp
@@ -5930,11 +5930,11 @@ bool CChar::_CanTick(bool fParentGoingToSleep) const
     //ADDTOCALLSTACK_DEBUG("CChar::_CanTick");
     EXC_TRY("Able to tick?");
 
-    if (IsDisconnected())
-	{
-        if (!IsTickableEvenIfDisconnected())
-            return false;
-	}
+    if (IsDisconnected() && !IsTickableEvenIfDisconnected())
+        return false;
+
+    if (IsTickableEvenIfDisconnected())
+        return true; // bypass sector sleep logic entirely
 
     return CObjBase::_CanTick(fParentGoingToSleep);
 
